a6e20600e81558a24a857d8372937ea9684413a0
Shubham-Mishra001 <shubham1999pandit@gmail.com>
2026-05-22 18:53:51 +0530

    fix vite host

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['keen-acceptance-production-85fb.up.railway.app']
+  }
 })
